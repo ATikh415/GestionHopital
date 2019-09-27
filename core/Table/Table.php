67a887be776a504179accbd18ea401e91df3f0dc
ATikh415 <atikhgueye1@gmail.com>
2019-09-27 18:29:48 +0000

    modifier methode query

--- core/Table/Table.php
+++ core/Table/Table.php
@@ -2,6 +2,8 @@
 
 namespace Core\Table;
 
+use Core\Database\Database;
+
 class Table{
 
     private $db;
@@ -9,13 +11,17 @@ class Table{
 
     public function __construct(Database $db)
     {
-        $this->bd = $db;
+        $this->db = $db;
         if($this->table === null){
             $parts = explode('\\', get_class($this));
             $class_name = str_replace('Table', '', end($parts));
             $this->table = strtolower($class_name) . 's';
         }
 
+    }
+
+    public function all(){
+    $this->query("SELECT * FROM {$this->table}");
     }
 
     public function query($sql, $attributes = null, $one = false){
@@ -27,10 +33,11 @@ class Table{
                     $one
             );
         }else{
-            return $this->db->prepare(
+            return $this->db->query(
                     $sql,
                     str_replace('Table', '', get_class($this)),
-                    $one);
+                    $one
+                );
         }
     }
 }
