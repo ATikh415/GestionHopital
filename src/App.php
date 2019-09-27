96e54ee3be1e5a8fb79dfda3b3e1597a0a7c426f
ATikh415 <atikhgueye1@gmail.com>
2019-09-27 16:40:40 +0000

    Ajout des methodes getTable() et getDb()

--- src/App.php
+++ src/App.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Core\Database\Database;
+
 class App{
 
     private static $instance;
@@ -12,5 +14,19 @@ class App{
             self::$instance = new App();
         }
         return self::$instance;
+    }
+
+    public function getTable($name){
+        $class_name = '\\App\\Table' . ucfirst($name) . 'Table';
+        return new $class_name($this->getDb());
+    }
+
+    public function getDb(){
+        $config = Config::getInstance(ROOT .'/config/config.php');
+        if($this->dbInstance === null){
+            $this->dbInstance = new Database($config->get('db_name'), $config->get('db_host'), $config->get('db_user'), $config->get('db_pass'));
+
+        }
+        return $this->dbInstance;
     }
 }
