d8009a08671cf1eef37aacce8dfc43abd16f5526
ATikh415 <atikhgueye1@gmail.com>
2019-09-29 00:33:56 +0000

    correction ok

--- core/Router.php
+++ core/Router.php
@@ -21,28 +21,28 @@ class Router{
         return $this;
     }
 
-    public function post(string $url, string $view, ?string $name = null): self
+    public function post(string $url, string $view, ?string $name = null):self
     {
         $this->router->map('POST', $url, $view, $name);
         return $this;
     }
 
-    public function match(string $url, string $view, ?string $name = null): self
+    public function match(string $url, string $view, ?string $name = null):self
     {
         $this->router->map('POST|GET', $url, $view, $name);
         return $this;
     }
 
-    public function url(string $url, $params = []): self
-    {
-         $this->router->generate($url, $params);
-        return $this;
+    public function url(string $name, array $params = []){
+        return $this->router->generate($name, $params );
     }
 
     public function run(): self
     {
         $match = $this->router->match();
-        $view = $match['target'];
+        $view = $match['target'] ?: 'e404';
+        $params = $match['params'];
+        $router = $this;
         \ob_start();
         require $this->viewPath . DIRECTORY_SEPARATOR . $view . '.php';
         $content = \ob_get_clean();
