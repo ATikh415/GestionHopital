f750d4d76b59cf9199cb1598cfb12de086209fc7
ATikh415 <atikhgueye1@gmail.com>
2019-09-29 00:36:03 +0000

    Month OK

--- src/Calendar/Month.php
+++ src/Calendar/Month.php
@@ -1,6 +1,10 @@
 <?php
 
+
 namespace App\Calendar;
+
+use DateTimeImmutable;
+use DateTimeInterface;
 
 class Month{
 
@@ -27,6 +31,57 @@ class Month{
 
     public function toString(): string{
         return $this->months[$this->month - 1] .' '. $this->year;
+    }
+
+    public function getWeeks(): int{
+        $start = $this->getStartingDay();
+      
+        $end = $start->modify(" +1 month -1 day");
+
+        $startWeek = intval($start->format('W'));
+        $endWeek = intval($end->format('W'));
+      
+        if($endWeek === 1){
+            $endWeek = \intval($end->modify('- 7 days')->format('W')) + 1;
+        }
+        
+        $week = $endWeek - $startWeek + 1;
+       
+        if($week < 0){
+            $week = \intval($end->format('W'));
+        }
+        return $week;
+    }
+
+    public function getStartingDay(): DateTimeInterface{
+        return new DateTimeImmutable("{$this->year}-{$this->month}-01");
+    }
+
+    public function withinMonth(DateTimeInterface $date): bool
+    {
+        return $this->getStartingDay()->format('Y-m') === $date->format('Y-m');
+    }
+
+    public function nextMonth(): Month
+    {
+        $month = $this->month + 1;
+        $year = $this->year;
+        if( $month > 12){
+            $month = 1;
+            $year += 1;
+        }
+        return new Month($month, $year);
+    }
+
+    public function previousMonth(): Month
+    {
+        $month = $this->month - 1;
+        $year = $this->year;
+        if( $month < 1){
+            $month = 12;
+            $year -= 1;
+        }
+        return new Month($month, $year);
     }
 
 }
