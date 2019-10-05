b04cb3d6d3d1dd2e4dc7280100cc74c5799b788f
ATikh415 <atikhgueye1@gmail.com>
2019-10-05 13:11:37 +0000

    Ajout des routes  patients edit, new, show, index

--- core/Validator/Validator.php
+++ core/Validator/Validator.php
@@ -2,6 +2,95 @@
 
 namespace Core\Validator;
 
+use DateTime;
+
 class Validator{
     
+    private $data;
+    protected $errors = [];
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function validates(array $data){
+        $this->data = $data;
+        $this->errors = [];
+        return $this->errors;
+    }
+
+    public function validate(string $field, string $method, ...$params): bool
+    {
+        if(empty($this->data[$field])){
+            $this->errors[$field] = "Le champ ne doit pas etre vide";
+            return false;
+        }else{
+            return call_user_func([$this, $method], $field, ...$params);
+        }
+    }
+
+    public function minLength(string $field, int $lenght): bool
+    {
+        if(mb_strlen($this->data[$field]) < $lenght){
+            $this->errors[$field] = "Le champ doit avoir plus de $lenght caracteres";
+            return false;
+        }
+        return true;
+    }
+
+    public function date($field): bool
+    {
+        if(DateTime::createFromFormat('Y-m-d', $this->data[$field]) === false){
+            $this->errors[$field] = "La date {$this->data[$field]} est invalide, recommencer";
+            return false;
+        }
+        return true;
+    }
+
+    public function dateDay($field){
+        if($this->date($field)){
+            $day = strftime('%A', strtotime($this->data[$field]));
+            if($day == 'Sunday' || $day == 'Saturday'){
+                $this->errors[$field] = "Les samedis et dimanches sont exclus, recommencer";
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function time($field): bool
+    {
+        if(DateTime::createFromFormat('H:i', $this->data[$field]) === false){
+            $this->errors[$field] = "La temps {$this->data[$field]} est invalide, recommencer";
+            return false;
+        }
+        return true;
+    }
+
+    public function beforeTime($startField, $endField): bool{
+        if($this->time($startField)  && $this->time($endField)){
+            $start =DateTime::createFromFormat('H:i', $this->data[$startField]);
+            $end =DateTime::createFromFormat('H:i', $this->data[$endField]);
+            if($start->getTimestamp() > $end->getTimestamp()){
+                $this->errors[$startField] = "L'heure de debut doit etre inferieur a l'heure de fin";
+                return false;
+            }
+            if(($end->format('i') - $start->format('i')) > 15){
+                $this->errors[$startField] = "Le rendz-vous ne peut pas duree plus de 15mn";
+                return false;
+            }
+            $st = intval($start->format('Y-m-d'));
+            $e = intval($end->format('H'));
+            if($st < 12 || $e > 15 ){
+                $this->errors[$startField] = "L'heure de rendez-vous est de 08h a 12h et de 15h a 17h";
+                return false;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
 }
--- public/index.php
+++ public/index.php
@@ -12,6 +12,20 @@ $whoops->prependHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 $router
+    //RENDEZ-VOUS
     ->get('/', 'rv/index', 'home')
+    ->get('/', 'e404', 'e404')
+    ->match('/rv/new', 'rv/new', 'rv_new')
+    ->post('/rv/delete', 'rv/delete', 'rv_delete')
+    ->match('/rv/edit/[i:id]', 'rv/edit', 'rv_edit')
+    ->match('/rv/show/[i:id]', 'rv/show', 'rv_show')
+    //SERVICES
     ->get('/service', 'service/index', 'service')
+    ->match('/service/new', 'service/new', 'service_new')
+    //PATIENTS
+    ->get('/patient', 'patients/index', 'patient')
+    ->match('/patient/new', 'patients/new', 'patient_new')
+    ->post('/patient/delete', 'patients/delete', 'patient_delete')
+    ->match('/patient/edit/[i:id]', 'patients/edit', 'patient_edit')
+    ->match('/patient/show/[i:id]', 'patients/show', 'patient_show')
     ->run();
